554b14c21d8e872b90687029af7e48353e47a84a
nitayke <nitayke@gmail.com>
2021-03-25 13:29:21 +0200

    code cleaning

--- scripts/post.py
+++ scripts/post.py
@@ -2,6 +2,8 @@ import requests
 import sys
 url = 'http://192.168.2.138/dbex/insert.php'
 
+# TODO: CHANGE IT TO YAML FILE
+
 myobj = {'Id': sys.argv[1], 'ffk_bit': sys.argv[2], 'fa_bit':sys.argv[3], 'localization_bit':sys.argv[4], 
 'maphandler_bit':sys.argv[5], 'mcu_bit':sys.argv[6], 'pathplanner_bit':sys.argv[7], 'waypoint_bit':sys.argv[8], 
 'wphandler_bit':sys.argv[9], 'mpc_bit':sys.argv[10], 'coverage_percentage':sys.argv[11], 
@@ -15,7 +17,7 @@ if len(arr)%2!=0:
 if len(arr) > 1 and len(arr)%2==0:
     for i in range(0, len(arr), 2):
         myobj[arr[i]] = arr[i+1]
-# print myobj
+
 x = requests.post(url, data = myobj)
 
-print(x.text)
+print x.text
