b1ccba013d46c94ea51fec2b0cd25fa64d6e8ee7
nitayke <nitayke@gmail.com>
2021-04-12 14:09:54 +0300

    get data.php

--- main.php
+++ main.php
@@ -12,36 +12,7 @@
 
 include './dbex/db_connect.php';
 include './variables.php';
-
-$conn = OpenCon();
-
-$slide = array_key_exists("table", $_GET) ? trim($_GET["table"]) : '';
-
-$result = mysqli_query($conn, "show tables");
-if ($slide == '')
-    $slide = mysqli_fetch_array($result)[0];
-$sqlQuery = "SELECT * FROM " . $slide;
-
-$resultSet = mysqli_query($conn, $sqlQuery) or die("<br>database error: ". mysqli_error($conn));
-
-
-$path = "./table.csv";
-
-$myfile = fopen($path, "w");
-
-$developer = mysqli_fetch_assoc($resultSet);
-if (!is_null($developer)) {
-	if ($developer['stats'] != '' && $developer['stats'] != '0') {
-		$arr=unserialize($developer ['stats']);
-		array_pop($developer);
-		foreach ($arr as $key => $val){
-			$developer[$key] = $val;
-		}
-	}
-	else
-		array_pop($developer);
-}
-
+include './get_data.php'
 
 ?>
 
