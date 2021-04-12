f30f7a25d71fa633db38ae6fd620421d51d30af8
nitayke <nitayke@gmail.com>
2021-04-12 13:56:16 +0300

    get_data.php added

--- get_data.php
+++ get_data.php
@@ -1,34 +1,29 @@
 <?php
-include './dbex/db_connect.php';
-include './variables.php';
 $conn = OpenCon();
-$filters = isset($_POST['parameter']) && isset($_POST['operator']) && isset($_POST['value']) &&
-$_POST['parameter'] !== '' && $_POST['operator'] !== '' && $_POST['value'] !== '';
 
 $slide = array_key_exists("table", $_GET) ? trim($_GET["table"]) : '';
 
 $result = mysqli_query($conn, "show tables");
 if ($slide == '')
     $slide = mysqli_fetch_array($result)[0];
-$sqlQuery = "SELECT " . implode(",", $parameters) . ", stats FROM " . $slide;
-if ($filters)
-{
-    if (array_key_exists($_POST['operator'], $operators_sql))
-        $operator = $operators_sql[$_POST['operator']];
-    else
-        $operator = $_POST['operator'];
+$sqlQuery = "SELECT * FROM " . $slide;
 
-    if ($_POST['parameter'] == 'ending_reason')
-        $_POST['value'] = '"' . $_POST['value'] . '"';
-    $sqlQuery = $sqlQuery . " WHERE " . $_POST['parameter'] . $operator . $_POST['value'];
-}
 $resultSet = mysqli_query($conn, $sqlQuery) or die("<br>database error: ". mysqli_error($conn));
 
+
+$path = "./table.csv";
+
+$myfile = fopen($path, "w");
+
 $developer = mysqli_fetch_assoc($resultSet);
-$updated_parameters = $parameters;
-if ($developer['stats'] != '' && $developer['stats'] != '0'){
+if ($developer['stats'] != '' && $developer['stats'] != '0') {
     $arr=unserialize($developer ['stats']);
-    foreach ($arr as $key => $val)
-        array_push($updated_parameters, $key);
+	array_pop($developer);
+	foreach ($arr as $key => $val){
+		$developer[$key] = $val;
+	}
 }
+else
+	array_pop($developer);
+
 ?>
--- main.php
+++ main.php
@@ -8,38 +8,11 @@
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 	<link href="style.css" rel="stylesheet" type="text/css">
 </head>
-<?php 
+<?php
 
 include './dbex/db_connect.php';
 include './variables.php';
-
-$conn = OpenCon();
-
-$slide = array_key_exists("table", $_GET) ? trim($_GET["table"]) : '';
-
-$result = mysqli_query($conn, "show tables");
-if ($slide == '')
-    $slide = mysqli_fetch_array($result)[0];
-$sqlQuery = "SELECT * FROM " . $slide;
-
-$resultSet = mysqli_query($conn, $sqlQuery) or die("<br>database error: ". mysqli_error($conn));
-
-
-$path = "./table.csv";
-
-$myfile = fopen($path, "w");
-
-$developer = mysqli_fetch_assoc($resultSet);
-if ($developer['stats'] != '' && $developer['stats'] != '0') {
-    $arr=unserialize($developer ['stats']);
-	array_pop($developer);
-	foreach ($arr as $key => $val){
-		$developer[$key] = $val;
-	}
-}
-else
-	array_pop($developer);
-
+include './get_data.php'
 
 ?>
 
