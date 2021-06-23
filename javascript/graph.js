e71b7822f80f57b74d9b13c360bb90324089b54e
nitayke <nitayke@gmail.com>
2021-06-23 12:24:21 +0300

    documentation of graph.js

--- javascript/graph.js
+++ javascript/graph.js
@@ -1,3 +1,4 @@
+// Used in the function set_range.
 function setParam(key, value) {
     let url = new URL(window.document.location);
     let params = new URLSearchParams(url.search.slice(1));
@@ -15,6 +16,8 @@ function set_range() {
     setParam('range', val)
 }
 
+
+
 var uri = window.location.toString()
 var url = new URL(uri)
 var params = new URLSearchParams(url.search)
@@ -31,29 +34,27 @@ var table = doc.getElementById('table')
 
 var index = -1
 
-for (var i = 0, row; row = table.rows[i]; i++) {
-    for (var j = 1, cell; cell = row.cells[j]; j++) {
-        if (i === 4 && cell.innerHTML === param.replaceAll('_', ' '))
-            index = j
-    }
-    if (index !== -1)
-        break
+// Finding the index of the chosen field
+for (var j = 1, cell; cell = table.rows[4].cells[j]; j++) {
+    if (cell.innerHTML === param.replaceAll('_', ' '))
+        index = j
 }
 
 var all_numbers = []
 var good = []
 var bad = []
 
+// Filling the arrays: all_numbers, good, and bad.
 for (var i = 0, row; row = table.rows[i]; i++) {
     if (row.cells[index] === undefined)
         continue
     const parsed = parseFloat(row.cells[index].innerHTML)
     if (!isNaN(parsed))
         all_numbers.push(parsed)
-    if (row.cells[0].style.background === "rgb(69, 255, 153)")
+    if (row.cells[0].style.background === "rgb(69, 255, 153)") // If the background color is green, add it to 'good' array
         good.push(parsed)
     else
-        bad.push(parsed)
+        bad.push(parsed) // If it's red
 }
 
 var min = Math.min.apply(null, all_numbers)
@@ -71,15 +72,23 @@ var good_y = []
 var bad_x = []
 var bad_y = []
 
-RANGE = Math.floor(max / RANGE)
+RANGE = Math.floor(max / RANGE) // now RANGE is how many points will be in the graph
 
 for (var i = 0; i < RANGE; i++) {
-    graph_x.push((max - min) * i / RANGE + min)
-    good_x.push((max - min) * i / RANGE + min)
-    bad_x.push((max - min) * i / RANGE + min)
+    // all of the graphs has the same x points
+    var x_value = (max - min) * i / RANGE + min
+    graph_x.push(x_value)
+    good_x.push(x_value)
+    bad_x.push(x_value)
+
     var sum = 0
     var good_sum = 0
     var bad_sum = 0
+
+    // sum = count of the values which are between two x points.
+    // for example, scenario time is 0 < x < 360, so if the range is 10,
+    // we iterate 36 times and check every time if the value is between
+    // 0-10, 10-20, etc.
     for (var j = 0; j < all_numbers.length; j++) {
         if (graph_x[i - 1] < all_numbers[j] && graph_x[i] > all_numbers[j])
             sum++;
